1a12856ef757d2fa7901df3f224f1549a14c4f67
libaozhong <libaozhong@xforceplus.com>
2021-02-04 13:55:03 +0800

    增加swapand contract

---
+++ call.sh
@@ -0,0 +1,73 @@
+curl http://127.0.0.1:9973 -H "Content-Type:application/json;charset=utf-8" -d \
+               '{
+                 "jsonrpc":"2.0",
+                 "id":1,
+                 "method":"author_insertKey",
+                 "params": [
+                   "aura",
+      "below extra hollow size razor miracle warrior you supreme useless travel kick",
+      "0xaab2ba71a1ec152cc382ce578d704fe1267a2e6b7b35d39a13550b4bc5543546"
+    ]
+  }'
+  sleep 1
+curl http://127.0.0.1:9973 -H "Content-Type:application/json;charset=utf-8" -d \
+               '{
+                 "jsonrpc":"2.0",
+                 "id":1,
+                 "method":"author_insertKey",
+                 "params": [
+                   "gran",
+      "below extra hollow size razor miracle warrior you supreme useless travel kick",
+      "0xded4fc2601d7c0f207dc5d7495f0ce67cd7348ed86acd9582df16e48872c2f17"
+    ]
+  }'
+  sleep 1
+curl http://127.0.0.1:9974 -H "Content-Type:application/json;charset=utf-8" -d \
+               '{
+                 "jsonrpc":"2.0",
+                 "id":1,
+                 "method":"author_insertKey",
+                 "params": [
+                   "aura",
+      "farm wait original sort north timber kit cake brick one kidney wheel",
+      "0xf2c4d2495fe61310b18376f5e86bba860104c96971e56907824adbd1956fd261"
+    ]
+  }'
+  sleep 1
+curl http://127.0.0.1:9974 -H "Content-Type:application/json;charset=utf-8" -d \
+               '{
+                 "jsonrpc":"2.0",
+                 "id":1,
+                 "method":"author_insertKey",
+                 "params": [
+                   "gran",
+      "farm wait original sort north timber kit cake brick one kidney wheel",
+      "0xb0d731ac88630155ffdf077eae805a5ea48c428d45cebf1a727cc150e839324b"
+    ]
+  }'
+
+sleep 1
+curl http://127.0.0.1:9975 -H "Content-Type:application/json;charset=utf-8" -d \
+               '{
+                 "jsonrpc":"2.0",
+                 "id":1,
+                 "method":"author_insertKey",
+                 "params": [
+                   "aura",
+      "finger between cream bicycle nerve teach inside mutual truck reflect fold dune",
+      "0xc0601cfc10be5714ebfb1b4b3b8988ab7e8a71a37b8f7505a54d9a5fde102653"
+    ]
+  }'
+  sleep 1
+curl http://127.0.0.1:9975 -H "Content-Type:application/json;charset=utf-8" -d \
+               '{
+                 "jsonrpc":"2.0",
+                 "id":1,
+                 "method":"author_insertKey",
+                 "params": [
+                   "gran",
+      "finger between cream bicycle nerve teach inside mutual truck reflect fold dune",
+      "0xb2ec08150bf382fca5bce9a25be8227fe92803cdc7c5189fb932c82ee9aa2f20"
+    ]
+  }'
+
---
+++ deploy.sh
@@ -0,0 +1,9 @@
+rm rf /tmp/st1
+rm rf /tmp/st2
+rm rf /tmp/st3
+rm rf /tmp/st4
+
+sudo nohup ./st1.sh >st1.log 2>&1 & 
+
+
+
